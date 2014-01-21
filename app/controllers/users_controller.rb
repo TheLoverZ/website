a55c29f86913726676a28c324363cbec2dc5365e
TheLover_Z <sethverlo5@gmail.com>
2014-01-21 18:36:22 +0800

    default signin_time set to 1.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -44,6 +44,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.save
+        @user.signin_times = 1
         sign_in @user
         format.html { redirect_to @user, notice: 'User was successfully created.' }
         format.json { render json: @user, status: :created, location: @user }
