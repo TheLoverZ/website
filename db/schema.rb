5e6705d29524af60509877ab8285ea989f30375b
TheLover_Z <sethverlo5@gmail.com>
2014-01-21 14:45:12 +0800

    total signin times added.

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140121063705) do
+ActiveRecord::Schema.define(:version => 20140121064422) do
 
   create_table "users", :force => true do |t|
     t.string   "username"
     t.string   "password"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
     t.string   "remember_token"
     t.string   "password_digest"
     t.integer  "signin_times"
     t.time     "last_visit"
+    t.integer  "total_signin_times"
   end
 
   add_index "users", ["remember_token"], :name => "index_users_on_remember_token"
