7f3a340163ad6b3d07949e6c1484c9c53eb3eff6
TheLover_Z <sethverlo5@gmail.com>
2014-01-21 13:32:22 +0800

    routes modified: add signup & signin etc.

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Website::Application.routes.draw do
   root :to => "users#index"
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
+  match '/signup', to: 'users#new', via: 'get'
+  match '/signin', to: 'sessions#new', via: 'get'
+  match '/signout', to: 'sessions#destroy', via: 'delete'
 
 end
