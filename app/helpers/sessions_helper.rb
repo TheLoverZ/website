0e2d63465a7b8b2ed3838a1f7062711905ddedf0
TheLover_Z <sethverlo5@gmail.com>
2014-01-21 15:21:53 +0800

    session helper modified: add last_visit & total_signin_times.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,11 +3,29 @@ module SessionsHelper
   def sign_in(user)
     remember_token = User.new_remember_token
     cookies.permanent[:remember_token] = remember_token
+    cookies.permanent[:last_visit] = Time.now
+    user.update_attribute(:last_visit, Time.now)
     user.update_attribute(:remember_token, User.encrypt(remember_token))
+    if user.signin_times.nil?
+      user_signin_times = 0
+    else
+      user_signin_times = user.signin_times
+    end
+    user.update_attribute(:signin_times, user_signin_times + 1)
     self.current_user = user
   end
 
   def signed_in?
+    if !current_user.nil?
+      current_user.update_attribute(:total_signin_times, 0) if current_user.total_signin_times.nil?
+    end
+    unless current_user.nil?
+      current_session_times =  (Time.now - cookies[:last_visit].to_time) / 60
+      current_user.total_signin_times += current_session_times
+      current_user.save
+    end
+    cookies.permanent[:last_visit] = Time.now
+    current_user.update_attribute(:last_visit, Time.now) unless current_user.nil?
     !current_user.nil?
   end
 
@@ -24,6 +42,14 @@ module SessionsHelper
     current_user.update_attribute(:remember_token, User.encrypt(User.new_remember_token))
     self.current_user = nil
     cookies.delete(:remember_token)
+  end
+
+  def current_user_numbers
+    total = 0
+    User.all.each do |u|
+      total += 1 if Time.now - u.last_visit < 60 * 5 # 5 分钟内访问过的
+    end
+    total
   end
 
 end
